6203fa7686eefc55ff3b1eabe77aad67bd244f25
Daniel <dev.danielcocos@gmail.com>
2025-02-16 21:20:26 +0000

    Update lualine.lua

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,9 +2,10 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
+    local transparent = require('settings.transparent-lualine')
 		require('lualine').setup({
 			options = {
-				theme = 'auto'
+        theme = transparent.theme(),
 			}
 		})
 	end
